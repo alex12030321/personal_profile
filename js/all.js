99bd735eaaa8bff896fac598f7929e70dd0dcda3
alex12030321 <alex12030321@hotmail.com>
2018-02-11 21:43:08 +0800

    add animate

--- js/all.js
+++ js/all.js
@@ -5,3 +5,23 @@ $('input[type="button"]').on('click', function() {
 $('#btn-menu a').on('click', function() {
     $('#btn-menu').hide();
 });
+
+$('.contactMeClick').on('click', function() {
+    $('html').animate({
+        scrollTop: $('a#contact').offset().top
+    }, 1000);
+});
+
+$('.portfolioClick').on('click', function() {
+    $('html').animate({
+        scrollTop: $('a#portfolio').offset().top
+    }, 1000);
+});
+
+$('.aboutClick').on('click', function() {
+    $('html').animate({
+        scrollTop: $('a#about').offset().top
+    }, 1000);
+});
+
+console.log($('a#contact').offset().top);
